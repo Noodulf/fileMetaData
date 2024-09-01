082e2bcb3ffddb8cc06d8a87d4ee93343474f1bd
noodulf <jaintarun926@gmail.com>
2024-09-01 19:39:45 +0530

    tests passed

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer');
 
 var app = express();
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
+const uploads = multer({ dest: '/uploads/' });
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.post("/api/fileanalyse",uploads.single("upfile"), (req, res)=>{
+  let file = req.file;
+  res.json({
+    name: file.originalname,
+    type: file.mimetype,
+    size:file.size
+  })
+})
 
 
 
